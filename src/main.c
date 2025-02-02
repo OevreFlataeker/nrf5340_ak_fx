fd3a91cff36ae12aabb91affd82009a51dbe08c5
Markus Dauberschmidt <daubsi@bigigloo.de>
2025-02-02 18:41:05 +0100

    More annotations and good fix for noise

--- src/main.c
+++ src/main.c
@@ -82,7 +82,7 @@ static int16_t zerowave[64] =
 	0,0,0,0,0,0,0,0,
 };
 
-#define I2S_BUFF_SIZE 256  // Define an appropriate buffer size
+#define I2S_BUFF_SIZE 8192  // Define an appropriate buffer size
 
 static int16_t rx_buffer[I2S_BUFF_SIZE];  // Buffer for received data
 static int16_t tx_buffer[I2S_BUFF_SIZE]; 
@@ -124,7 +124,7 @@ static int nrfadk_i2s_reg_init(void)
 	NRF_I2S0->CONFIG.CLKCONFIG =    I2S_CONFIG_CLKCONFIG_CLKSRC_ACLK;
 	NRF_I2S0->CONFIG.MCKFREQ =      MCKFREQ_6_144_MHZ;
 	NRF_I2S0->CONFIG.RATIO =        I2S_CONFIG_RATIO_RATIO_128X;
-	NRF_I2S0->CONFIG.CHANNELS =     I2S_CONFIG_CHANNELS_CHANNELS_Left;	
+	NRF_I2S0->CONFIG.CHANNELS =     I2S_CONFIG_CHANNELS_CHANNELS_Stereo; // if I2S_CONFIG_CHANNELS_CHANNELS_Left the left and right channel will be the same	
 	NRF_I2S0->CONFIG.TXEN =         I2S_CONFIG_TXEN_TXEN_Enabled;
 	NRF_I2S0->CONFIG.RXEN = 		I2S_CONFIG_RXEN_RXEN_Enabled;  // Enable RX (new)
 
@@ -168,7 +168,7 @@ void i2s_polling_loop(void)
         // Wait for new RX data
         while (NRF_I2S0->EVENTS_RXPTRUPD == 0);
 		
-		//printk("\nEvents: %d\n", cnt);
+		// printk("\nEvents: %d\n", NRF_I2S0->EVENTS_RXPTRUPD);
         // Clear RX event
         NRF_I2S0->EVENTS_RXPTRUPD = 0;
 
@@ -179,7 +179,7 @@ void i2s_polling_loop(void)
         //NRF_I2S0->TXD.PTR = (uint32_t)&sine750_48k_16b_1c[0];//(uint32_t)tx_buffer;
 		//NRF_I2S0->RXTXD.MAXCNT =        (sizeof(sine750_48k_16b_1c)) / (sizeof(uint32_t));
         // Ensure we restart I2S for continuous operation
-        NRF_I2S0->TASKS_START = I2S_TASKS_START_TASKS_START_Trigger;
+        // NRF_I2S0->TASKS_START = I2S_TASKS_START_TASKS_START_Trigger;
 		while (NRF_I2S0->EVENTS_TXPTRUPD == 0);
     }
 }
@@ -206,23 +206,68 @@ static cs47l63_t cs47l63_driver;
 static const uint32_t cs47l63_cfg[][2] =
 {
 
-	// Audio Serial Port 1 (I2S slave, 48kHz 16bit, Left mono, RX and TX)
-	{ CS47L63_ASP1_CONTROL2,
-		(0x10  << CS47L63_ASP1_RX_WIDTH_SHIFT) |        // 16bit
-		(0x10  << CS47L63_ASP1_TX_WIDTH_SHIFT) |        // 16bit
-		(0b010 << CS47L63_ASP1_FMT_SHIFT)               // I2S
+#undef i2s_echo_ASP1
+#ifdef i2s_echo_ASP1
+	// Uses sample rate of 48kHz instead of 16kHz like below!
+	/* Enable ASP1 GPIOs */
+	{ CS47L63_GPIO1_CTRL1, 0xE1000000 }, // ASP1_DOUT, Sets the default config of GPIO1 (ASP1 interface) to Non-GPIO mode GPn_FN = 0, rest ignored, page 153
+	{ CS47L63_GPIO2_CTRL1, 0xE1000000 }, // ASP1_DIN, Sets the default config of GPIO2 (ASP1 interface) to Non-GPIO mode GPn_FN = 0, rest ignored, 
+	{ CS47L63_GPIO3_CTRL1, 0xE1000000 }, // ASP1_BCLK, Sets the default config of GPIO3 (ASP1 interface) to Non-GPIO mode GPn_FN = 0, rest ignored, 
+	{ CS47L63_GPIO4_CTRL1, 0xE1000000 }, // ASP1_FSYNC, Sets the default config of GPIO4 (ASP1 interface) to Non-GPIO mode GPn_FN = 0, rest ignored, 
+
+    /* Set correct sample rate */
+	{ CS47L63_SAMPLE_RATE1, 0x000000012 }, // p125, 4.10.2., p132, 0x12 == 16 kHz -> p125, 4.10.3.1, requires SYSCLK to be 98.304MHz, SYSCLK_FREQ=100, SYSCLK_FRAC=0
+	                                       // default is 0x03 == 48 kHz
+
+	/* Disable unused sample rates */
+	{ CS47L63_SAMPLE_RATE2, 0 }, // p132, None (default is 0)
+	{ CS47L63_SAMPLE_RATE3, 0 }, // p132, None (default is 0)
+	{ CS47L63_SAMPLE_RATE4, 0 }, // p132, None (default is 0)
+
+	/* Set ASP1 in slave mode and 16 bit per channel */
+	{ CS47L63_ASP1_CONTROL2, 0x10100200 }, // 00010000 00010000 00000010 00000000. p115
+										   // Bit 0 (ASP1_FSYNC_MSTR) = ASP1_FSYNC Slave Mode
+										   // Bit 1 (ASP1_FSYNC_FRC) = Normal
+										   // Bit 2 (ASP1_FSYNC_INV) = ASP1_FSYNC not inverted
+										   // Bit 4 (ASP1_BCLK_MSTR) = ASP1_BCLK Slave Mode
+										   // Bit 5 (ASP1_BCLK_FRC) = Normal
+										   // Bit 6 (ASP1_BCLK_INV)  = ASP1_BCLK not inverted
+										   // Bit 8-10 = 0b010 = I2S Mode (p118) (is default)
+										   // Bit 16-23 = 0b10000 = 0x10 = 16 = ASP1 TX Slot Width (Number of BCLK cycles per slot), default is 0x18 = 24 bit
+										   // Bit 24-31 = 0b10000 = 0x10 = 16 = ASP1 RX Slot Width (Number of BCLK cycles per slot), default is 0x18 = 24 bit
+	
+	{ CS47L63_ASP1_CONTROL3, 0x0000 }, // Always 0, p120, ASP1_DOUT Tristate Control, 00 = Logic 0 during unused time slots, Logic 0 if all transmit channels are disabled
+	{ CS47L63_ASP1_DATA_CONTROL1, 0x0020 }, // p118, 0x20 = 32, ASP1 TX Data Width (Number of valid data bits per slot), is default
+	{ CS47L63_ASP1_DATA_CONTROL5, 0x0020 }, // p118, 0x20 = 32, ASP1 RX Data Width (Number of valid data bits per slot), is default
+	{ CS47L63_ASP1_ENABLES1, 0x30003 },	  // 0011 00000000 00000011, p116
+										  // Bit 0 = 1 = ASP1_TX1_EN
+										  // Bit 1 = 1 = ASP1_TX2_EN
+										  // Bit 16 = 1 = ASP1_RX1_EN
+										  // Bit 17 = 1 = ASP1_RX2_EN
+	// What is different?
+	// (No change from default: Additional configuration of ASP1 RX/TX Data Width in CS47L63_ASP1_DATA_CONTROL1, CS47L63_ASP1_DATA_CONTROL5 to 0x20 = 32, default is 0x20)
+	// Additional activation of ASP1_TX2 and ASP1_RX2 in CS47L63_ASP1_ENABLES1
+	// Explicit configuration of GPIO ports for alternate services (default is GPIO and not alternate services!)
+	// !! Explicit configuration of sample rate in CS47L63_SAMPLE_RATE1,2,3,4 to 16kHz (0x12), default is 48kHz (0x0x3) !!
+#else
+	// Audio Serial Port 1 (I2S slave, 48kHz 16bit, Left mono, RX)
+	{ CS47L63_ASP1_CONTROL2,							// 0x101000200 (matches above)
+		(0x10  << CS47L63_ASP1_RX_WIDTH_SHIFT) |        // 16bit (default is 0x18 = 24 bit)
+		(0x10  << CS47L63_ASP1_TX_WIDTH_SHIFT) |        // 16bit (default is 0x18 = 24 bit)
+		(0b010 << CS47L63_ASP1_FMT_SHIFT)               // I2S (is default)
 	},
 	{ CS47L63_ASP1_CONTROL3,
-		(0b00 << CS47L63_ASP1_DOUT_HIZ_CTRL_SHIFT)      // Always 0
+		(0b00 << CS47L63_ASP1_DOUT_HIZ_CTRL_SHIFT)      // Always 0 (matches above)
 	},
 	// Enable the various channels for RX and TX
-	{ CS47L63_ASP1_ENABLES1,
-		(0 << CS47L63_ASP1_RX2_EN_SHIFT) |              // Disabled
+	{ CS47L63_ASP1_ENABLES1,							// 0x10001 (no match)
+		(1 << CS47L63_ASP1_RX2_EN_SHIFT) |              // Disabled
 		(1 << CS47L63_ASP1_RX1_EN_SHIFT) |              // Enabled
-		(0 << CS47L63_ASP1_TX2_EN_SHIFT) |              // Disabled
-		//(0 << CS47L63_ASP1_TX1_EN_SHIFT)                // Disabled
+		(1 << CS47L63_ASP1_TX2_EN_SHIFT) |              // Disabled
+		//(0 << CS47L63_ASP1_TX1_EN_SHIFT)               // Disabled
 		(1 << CS47L63_ASP1_TX1_EN_SHIFT)                // Enabled
 	},
+#endif
 #ifdef ENABLE_MIC
 	// Enable digital MIC
 	/* Set MICBIASes */
@@ -279,12 +324,12 @@ static const uint32_t cs47l63_cfg[][2] =
 		(0x2B  << CS47L63_OUT1LMIX_VOL1_SHIFT) |        // quite weak
 		(0x020 << CS47L63_OUT1L_SRC1_SHIFT)             // ASP1_RX1 // from MCU (currently a sine wave only)
 	},
-/*
+
 	{ CS47L63_OUT1L_INPUT2,
-		(0x2B  << CS47L63_OUT1LMIX_VOL2_SHIFT) |        // 0dB
+		(0x2B  << CS47L63_OUT1LMIX_VOL2_SHIFT) |        // quite weak
 		(0x021 << CS47L63_OUT1L_SRC2_SHIFT)             // ASP1_RX2
 	},
-*/	
+
 #ifdef COMMENTS
 	//{ CS47L63_OUT1L_INPUT3,
 	//	(0x2B  << CS47L63_OUT1LMIX_VOL3_SHIFT) |        // -21dB
@@ -362,6 +407,41 @@ static int nrfadk_hwcodec_config(const uint32_t config[][2], uint32_t length)
 	return CS47L63_STATUS_OK;
 }
 
+// Unused in minimum example
+// Used in i2s_echo
+// clocks are different from our barebone example (see comments for nrfadk_hwcodec_init())
+const uint32_t clock_configuration[][2] = {
+	{ CS47L63_SAMPLE_RATE3, 0x0012 }, // p125, p132, 16kHz
+	{ CS47L63_SAMPLE_RATE2, 0x0002 }, // p125, p132, 24kHz
+	{ CS47L63_SAMPLE_RATE1, 0x0003 }, // p125, p132, 48kHz
+	{ CS47L63_SYSTEM_CLOCK1, 0x034C }, // p131, 0b0011 01001100, Bit 8-10: 0b011 = 49.152 MHz, Bit 6 = SYSCLK_EN, Bit 0-4 = 0x00 = MCLK1
+	{ CS47L63_ASYNC_CLOCK1, 0x034C }, // p132, 0b0011 01001100, Bit 8-10: 0b011 = 49.152 MHz, Bit 6 = ASYNCCLK_EN, Bit 0-4 = 0x00 = MCLK1
+	{ CS47L63_FLL1_CONTROL2, 0x88200008 }, // p139, 0b10001000 00100000 00000000 00001000, Bit 0-9 = FLL1 Integer multiply for FREF, default 0x4, Bit 21 = FLL1 Reference Detect control, enabled (default), Bit 27 = FLL1_LOCKDET, enabled (default), Bit 28-31 = FLL1 Lock Detect threshold = 0x8 = default
+	{ CS47L63_FLL1_CONTROL3, 0x10000 }, // p139, FLL1 Fractional multiply for FREF = 0x1, default is 0x0
+	{ CS47L63_FLL1_GPIO_CLOCK, 0x0005 }, // p139, FLL1_GPCLK_DIV = 0x02, default
+	{ CS47L63_FLL1_CONTROL1, 0x0001 }, // p139, FLL1 enable
+};
+
+// Buggy configuration - leaves ASP2 unconfigured in default GPIO mode
+const uint32_t GPIO_configuration[][2] = {
+	{ CS47L63_GPIO6_CTRL1, 0x61000001 }, // ASP2
+	{ CS47L63_GPIO7_CTRL1, 0x61000001 }, // ASP2
+	{ CS47L63_GPIO8_CTRL1, 0x61000001 }, // ASP2
+
+	/* Enable CODEC LED */
+	{ CS47L63_GPIO10_CTRL1, 0x41008001 },
+};
+
+const uint32_t FLL_toggle[][2] = {
+	{ CS47L63_FLL1_CONTROL1, 0x0000 }, // p139, FLL1 disable
+	{ 0x01, 1000 },                    // wait 1000ms
+	{ CS47L63_FLL1_CONTROL1, 0x0001 }, // p139, FLL1 enable
+};
+
+const uint32_t soft_reset[][2] = {
+	{ CS47L63_SFT_RESET, 0x5A000000 }, // p168, write 0x5A to bits 24-31 to perform softreset
+	{ 0x01, 3000 },                    // wait 3000ms
+};
 
 /**
  * @brief       Initialize the CS47L63, start clocks, and configure subsystems.
@@ -385,7 +465,7 @@ static int nrfadk_hwcodec_init(void)
 	// Initialize driver
 	ret += cs47l63_comm_init(&cs47l63_driver);
 
-
+	
 	// Start FLL1 and SYSCLK
 	ret += cs47l63_fll_config(&cs47l63_driver, CS47L63_FLL1,
 	                          CS47L63_FLL_SRC_MCLK1, 6144000, 49152000);
@@ -396,6 +476,9 @@ static int nrfadk_hwcodec_init(void)
 
 	ret += cs47l63_update_reg(&cs47l63_driver, CS47L63_SYSTEM_CLOCK1,
 	                          CS47L63_SYSCLK_EN_MASK, CS47L63_SYSCLK_EN);
+	
+	//nrfadk_hwcodec_config(clock_configuration, ARRAY_SIZE(clock_configuration));
+	nrfadk_hwcodec_config(GPIO_configuration, ARRAY_SIZE(GPIO_configuration));
 
 
 	// Configure subsystems
